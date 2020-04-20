370b214bbf5a872e35324146790fd1c128eb5e8e
Rajendra Primadama <rajendragustiprimadama@gmail.com>
2020-04-20 10:51:26 +0700

    200420 - update general js

--- assets/js/general.php
+++ assets/js/general.php
@@ -251,4 +251,19 @@
         s = addSeparatorsNF(harga, '.', '.', ',');
         return s;
     }
+
+    function addSeparatorsNF(nStr, inD, outD, sep){
+        nStr += '';
+        var dpos = nStr.indexOf(inD);
+        var nStrEnd = '';
+        if (dpos != -1) {
+            nStrEnd = outD + nStr.substring(dpos + 1, nStr.length);
+            nStr = nStr.substring(0, dpos);
+        }
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(nStr)) {
+            nStr = nStr.replace(rgx, '$1' + sep + '$2');
+        }
+        return nStr + nStrEnd;
+    }
 </script>
